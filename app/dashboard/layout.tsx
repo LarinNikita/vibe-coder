46ad8865c0a45c612fec9f047a72de7ba7f8177b
Larin Nikita <l_nikita2000@mail.ru>
2025-10-04 14:25:43 +0700

    sidebar ui

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import DashboardSidebar from '@/features/dashboard/components/dashboard-sidebar';
+
+import { SidebarProvider } from '@/components/ui/sidebar';
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full overflow-x-hidden">
+        <DashboardSidebar initialPlaygroundData={[]} />
+        <main className="flex-1">{children}</main>
+      </div>
+    </SidebarProvider>
+  );
+}
