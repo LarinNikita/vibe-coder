74dce6d6eaa91eaf3e95b7a2cabce1fb3182d374
Larin Nikita <l_nikita2000@mail.ru>
2025-10-04 13:43:49 +0700

    landing page

---
+++ components/ui/theme-toggle.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { useTheme } from 'next-themes';
+import { Moon, Sun } from 'lucide-react';
+
+export function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <div
+      className="cursor-pointer"
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
+      {theme === 'dark' ? <Sun /> : <Moon />}
+    </div>
+  );
+}
