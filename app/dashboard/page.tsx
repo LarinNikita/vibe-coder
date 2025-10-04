5ff99bafe42476b9465fe8f9d12afd31670cf9dd
Larin Nikita <l_nikita2000@mail.ru>
2025-10-04 15:16:09 +0700

    dashboard action buttons ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,55 @@
+import Image from 'next/image';
+import { Plus } from 'lucide-react';
+
+import AddNewButton from '@/features/dashboard/components/add-new-button';
+import AddRepoButton from '@/features/dashboard/components/add-repo-button';
+
+import { Button } from '@/components/ui/button';
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyTitle,
+} from '@/components/ui/empty';
+
 export default function Page() {
-  return <div>Page</div>;
+  const playgrounds: any[] = [];
+
+  return (
+    <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
+        <AddNewButton />
+        <AddRepoButton />
+      </div>
+      <div className="mt-10 flex-col flex justify-center items-center w-full">
+        {playgrounds && playgrounds.length === 0 ? (
+          <Empty>
+            <EmptyHeader>
+              <Image
+                src={'/empty-state.svg'}
+                alt="Empty"
+                width={90}
+                height={90}
+                className="size-48"
+              />
+            </EmptyHeader>
+            <EmptyTitle>No projects found</EmptyTitle>
+            <EmptyDescription>
+              Create a new project to get started
+            </EmptyDescription>
+            <EmptyContent>
+              <Button>
+                <Plus className="size-4" />
+                Create a new project
+              </Button>
+            </EmptyContent>
+          </Empty>
+        ) : (
+          // TODO add playground table
+          <p>No Playgrounds</p>
+        )}
+      </div>
+    </div>
+  );
 }
