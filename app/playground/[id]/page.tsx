1d488ea2cd3421dc2d1df656a0836b4ffcbbe491
Larin Nikita <l_nikita2000@mail.ru>
2025-10-11 14:56:42 +0700

    file explorer ui

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -2,10 +2,13 @@
 
 import { useParams } from 'next/navigation';
 
+import { usePlayground } from '@/features/playground/hooks/usePlayground';
+import { useFileExplorer } from '@/features/playground/hooks/useFileExplorer';
+import TemplateFileFree from '@/features/playground/components/template-file-tree';
+
 import { Separator } from '@/components/ui/separator';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
-import { usePlayground } from '@/features/playground/hooks/usePlayground';
 
 export default function Page() {
   const { id } = useParams<{ id: string }>();
@@ -17,10 +20,30 @@ export default function Page() {
     loadPlayground,
     saveTemplateData,
   } = usePlayground(id);
+  const {
+    activeFileId,
+    closeAllFiles,
+    openFile,
+    closeFile,
+    editorContent,
+    updateFileContent,
+    handleAddFile,
+    handleAddFolder,
+    handleDeleteFile,
+    handleDeleteFolder,
+    handleRenameFile,
+    handleRenameFolder,
+    openFiles,
+    setTemplateData,
+    setActiveFileId,
+    setPlaygroundId,
+    setOpenFiles,
+  } = useFileExplorer();
 
   return (
     <TooltipProvider>
       <>
+        <TemplateFileFree data={templateData!} />
         <SidebarInset>
           <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
             <SidebarTrigger className="-ml-1" />
--- lib/template.ts
+++ lib/template.ts
@@ -1,6 +1,6 @@
 export const templatePaths = {
   REACT: '/vibecoder-starters/react-ts',
-  NEXTJS: '/vibecoder-starters/nextjs-new',
+  NEXTJS: '/vibecoder-starters/nextjs-shadcn',
   EXPRESS: '/vibecoder-starters/express-simple',
   VUE: '/vibecoder-starters/vue',
   HONO: '/vibecoder-starters/hono-nodejs-starter',
