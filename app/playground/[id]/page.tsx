4c62d6929b42e35f592e51d2fc3feef8cb2d0850
Larin Nikita <l_nikita2000@mail.ru>
2025-10-11 12:27:21 +0700

    loaded template in file-explorer

---
+++ app/playground/[id]/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+
+import { Separator } from '@/components/ui/separator';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
+import { usePlayground } from '@/features/playground/hooks/usePlayground';
+
+export default function Page() {
+  const { id } = useParams<{ id: string }>();
+  const {
+    playgroundData,
+    templateData,
+    isLoading,
+    error,
+    loadPlayground,
+    saveTemplateData,
+  } = usePlayground(id);
+
+  return (
+    <TooltipProvider>
+      <>
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <div className="flex flex-1 items-center gap-2">
+              <div className="flex flex-col flex-1">
+                {playgroundData?.title || 'Code Playground'}
+              </div>
+            </div>
+          </header>
+        </SidebarInset>
+      </>
+    </TooltipProvider>
+  );
+}
